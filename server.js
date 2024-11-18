efd29b61415c25353e5ddc1bc7d24c572f9eac0a
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2024-11-18 08:07:54 -0300

    Cors policy

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // lucasdev - uKs6kh3SurdgYCFu
 import express from 'express';
+import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post('/users', async (req, res) => {
   await prisma.user.create({
@@ -15,7 +17,7 @@ app.post('/users', async (req, res) => {
       password: req.body.password, 
     }
   });
-  res.status(201).send('O post deu certo!');
+  res.status(201).json({message: 'O post deu certo!'});
 });
 
 app.get('/users', async (req, res) => {
@@ -34,21 +36,16 @@ app.put('/users/:id', async (req, res) => {
       password: req.body.password,
     }
   });
-  res.status(201).send(`Os dados de ${req.body.name} foram alterados.`);
+  res.status(201).json({message: `Os dados de ${req.body.name} foram alterados.`});
 });
 
 app.delete('/users/:id', async (req, res) => {
-  await prisma.user.update({
+  await prisma.user.delete({
     where: {
       id: req.params.id,
-    },
-    data: {
-      email:req.body.email,
-      name: req.body.name,
-      password: req.body.password,
     }
   });
+  res.status(200).json({message: "Usuário deletado com sucesso!"});
 });
-
 
 app.listen(3000);
