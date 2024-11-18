c377e6cc0dacdf8325593e7c7aec0c46cf1f536d
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2024-11-18 07:36:33 -0300

    Put method

--- server.js
+++ server.js
@@ -1,18 +1,53 @@
 // lucasdev - uKs6kh3SurdgYCFu
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
-const users = [];
-
-app.post('/users', (req, res) => {
-  users.push(req.body);
+app.post('/users', async (req, res) => {
+  await prisma.user.create({
+    data: {
+      email:req.body.email,
+      name: req.body.name,
+      password: req.body.password, 
+    }
+  });
   res.status(201).send('O post deu certo!');
 });
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
   res.status(200).json(users);
+});
+
+app.put('/users/:id', async (req, res) => {
+  await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      email:req.body.email,
+      name: req.body.name,
+      password: req.body.password,
+    }
+  });
+  res.status(201).send(`Os dados de ${req.body.name} foram alterados.`);
+});
+
+app.delete('/users/:id', async (req, res) => {
+  await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      email:req.body.email,
+      name: req.body.name,
+      password: req.body.password,
+    }
+  });
 });
 
 
