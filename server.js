fa2ea92e8d26df4cd117d955025c7ca86439c356
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2024-11-18 06:36:33 -0300

    Prisma and env config

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+// lucasdev - uKs6kh3SurdgYCFu
 import express from 'express';
 
 const app = express();
-app.get('/users', (req, res) => {
-  res.send('Deu muito bom!!!');
+app.use(express.json());
+
+const users = [];
+
+app.post('/users', (req, res) => {
+  users.push(req.body);
+  res.status(201).send('O post deu certo!');
 });
+
+app.get('/users', (req, res) => {
+  res.status(200).json(users);
+});
+
 
 app.listen(3000);
